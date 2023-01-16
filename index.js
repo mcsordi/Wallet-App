2befe6f52ec3c0172c8b9c231c7f89515d46ac74
mcsordi <matheuscarissordi@gmail.com>
2023-01-16 20:06:31 -0300

    feat: login screen and cadaster screen, local storage done, initialized home screen

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-onclicknewpage = function () {
-  window.open();
+const saveEmail = () => {
+  const email = document.getElementById("input-email").value;
+  if (email.lenght < 5 || !email.includes("@") || !email.includes(".")) {
+    alert("Email invalido");
+    return;
+  }
+  localStorage.setItem("@WalletApp:email", email);
+  window.open("./pages/register/index.html", "_self");
 };
