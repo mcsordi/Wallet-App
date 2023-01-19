0d34708ef8759641443a9b333d4e9f852d018202
mcsordi <matheuscarissordi@gmail.com>
2023-01-19 20:16:10 -0300

    feat: users validation login implemented

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const saveEmail = () => {
+const validadeUser = async (email) => {
+  try {
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+    );
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
+
+const onclickLogin = async () => {
   const email = document.getElementById("input-email").value;
-  if (email.lenght < 5 || !email.includes("@") || !email.includes(".")) {
-    alert("Email invalido");
+  if (email.length < 5 || !email.includes("@") || !email.includes(".")) {
+    alert("Email Invalido");
     return;
   }
-  localStorage.setItem("@WalletApp:email", email);
-  window.open("./pages/register/index.html", "_self");
+  const result = await validadeUser(email);
+  if (result.error) {
+    alert("Falha ao validar e-mail.");
+    return;
+  }
+
+  localStorage.setItem("@WalletApp:email", result.email);
+  localStorage.setItem("@WalletApp:name", result.name);
+  localStorage.setItem("@WalletApp:id", result.id);
+  window.open("./src/image/register/index.html", "_self");
 };
